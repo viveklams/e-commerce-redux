4f61914cdcb5a110a91f49dd76de4b0db26a1410
viveklams <viveklamgadey@gmail.com>
2024-10-20 18:03:26 +0530

    added routes for admin dashboard features orders and products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import AuthRegister from "./pages/auth/register";
 import AuthLogin from "./pages/auth/login";
 import AuthLayout from "./components/auth/layout";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminFeatures from "./pages/admin-view/features";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminProducts from "./pages/admin-view/products";
 
 function App() {
   return (
@@ -10,6 +15,12 @@ function App() {
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
+        </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="features" element={<AdminFeatures />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="products" element={<AdminProducts />} />
         </Route>
       </Routes>
     </div>
---
+++ frontend/src/pages/admin-view/features.jsx
@@ -0,0 +1,5 @@
+const AdminFeatures = () => {
+  return <div>AdminFeatures</div>;
+};
+
+export default AdminFeatures;
