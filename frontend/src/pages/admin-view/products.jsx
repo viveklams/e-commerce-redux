89d1161aefbf70a06c685b268d66cb55db137218
viveklams <viveklamgadey@gmail.com>
2024-10-24 13:16:28 +0530

    Admin products form has been designed and created

--- frontend/src/pages/admin-view/products.jsx
+++ frontend/src/pages/admin-view/products.jsx
@@ -1,5 +1,65 @@
+import CommonForm from "@/components/common/form";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { addProductFormElements } from "@/config";
+import { Fragment, useState } from "react";
+
+const initialFormData = {
+  image: null,
+  title: "",
+  description: "",
+  category: "",
+  brand: "",
+  price: "",
+  salePrice: "",
+  totalStock: "",
+  averageReview: 0,
+};
+
 const AdminProducts = () => {
-  return <div>AdminProducts</div>;
+  const [openCreateProductsDialog, setOpenCreateProductsDialog] =
+    useState(false);
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  function onSubmit() {}
+
+  return (
+    <Fragment>
+      <div className="mb-5 w-full flex justify-end">
+        <Button onClick={() => setOpenCreateProductsDialog(true)}>
+          Add New Product
+        </Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet
+        open={openCreateProductsDialog}
+        onOpenChange={() => {
+          setOpenCreateProductsDialog(false);
+        }}
+      >
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>
+            <SheetTitle>Add New Product</SheetTitle>
+          </SheetHeader>
+          <div className="py-6">
+            <CommonForm
+              onSubmit={onSubmit}
+              formData={formData}
+              setFormData={setFormData}
+              buttonText="Add"
+              formControls={addProductFormElements}
+            />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </Fragment>
+  );
 };
 
 export default AdminProducts;
