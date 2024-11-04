5d1a35b18dc6683a3ea67761bb2470c2b46fef3f
viveklams <viveklamgadey@gmail.com>
2024-11-04 09:44:29 +0530

    folder structture created for product-Tile in shopping view

--- frontend/src/pages/shopping-view/listing.jsx
+++ frontend/src/pages/shopping-view/listing.jsx
@@ -45,6 +45,7 @@ const ShoppingListing = () => {
             </DropdownMenu>
           </div>
         </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4"></div>
       </div>
     </div>
   );
---
+++ frontend/src/pages/shopping-view/product-tile.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable react/prop-types */
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+
+function ShoppingProductTile({ product }) {
+  return (
+    <Card className="w-full max-w-sm mx-auto">
+      <div>
+        <div className="relative">
+          <img
+            src={product?.image}
+            alt={product?.title}
+            className="w-full h-[300px] object-cover rounded-t-lg"
+          />
+          {product?.salePrice > 0 ? (
+            <Badge className="absolute top-2 left-2 bg-red-500 hover:bg-red-600">
+              Sale
+            </Badge>
+          ) : null}
+        </div>
+        <CardContent className="p-4">
+          <h2 className="text-xl font-bold mb-2">{product?.title}</h2>
+          <div className="flex justify-between items-center mb-2">
+            <span className="text-[16px] text-muted-foreground">
+              {product?.category}
+            </span>
+            <span className="text-[16px] text-muted-foreground">
+              {product?.brand}
+            </span>
+          </div>
+
+          <div className="flex justify-between items-center mb-2">
+            <span
+              className={`${
+                product?.salePrice > 0 ? "line-through" : ""
+              } text-lg font-semibold text-primary`}
+            >
+              {product?.price}
+            </span>
+            {product?.salePrice > 0 ? (
+              <span className="text-lg font-semibold text-primary">
+                ₹{product?.salePrice}
+              </span>
+            ) : null}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Add to Cart</Button>
+        </CardFooter>
+      </div>
+    </Card>
+  );
+}
+
+export default ShoppingProductTile;
