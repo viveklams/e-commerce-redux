282559d58fcfbdb18e7b86cfa4308a31619d852c
viveklams <viveklamgadey@gmail.com>
2024-11-04 09:21:54 +0530

    product listing page and component filter done

---
+++ frontend/src/components/shopping-view/filter.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/jsx-key */
+import { filterOptions } from "@/config";
+import { Fragment } from "react";
+import { Label } from "../ui/label";
+import { Checkbox } from "../ui/checkbox";
+import { Separator } from "../ui/separator";
+
+const Productfilter = () => {
+  return (
+    <div className="bg-background rounded-lg shadow-sm">
+      <div className="p-4 border-b">
+        <h2 className="text-lg font-semibold">Filters</h2>
+      </div>
+      <div className="p-4 space-y-4">
+        {Object.keys(filterOptions).map((keyItem) => (
+          <Fragment key={keyItem}>
+            <div>
+              <h3 className="text-base font-bold">{keyItem}</h3>
+              <div className="grid gap-2 mt-2">
+                {filterOptions[keyItem].map((option) => (
+                  <Label className="flex font-medium items-center gap-2 ">
+                    <Checkbox />
+                    {option.label}
+                  </Label>
+                ))}
+              </div>
+            </div>
+            <Separator />
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Productfilter;
--- frontend/src/pages/shopping-view/listing.jsx
+++ frontend/src/pages/shopping-view/listing.jsx
@@ -1,5 +1,53 @@
+import Productfilter from "@/components/shopping-view/filter";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { sortOptions } from "@/config";
+import { ArrowUpDownIcon } from "lucide-react";
+
 const ShoppingListing = () => {
-  return <div>Shopping listing page</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
+      <Productfilter />
+      <div className="bg-background w-full rounded-lg shadow-sm">
+        <div className="p-4 border-b flex items-center justify-between">
+          <h2 className="text-lg font-extrabold">All Products</h2>
+          <div className="flex items-center gap-3">
+            <span className="text-muted-foreground">10 Products</span>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="flex items-center gap-1"
+                >
+                  <ArrowUpDownIcon className="h-4 w-4" />
+                  <span>Sort by</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-[200px]">
+                <DropdownMenuRadioGroup>
+                  {sortOptions.map((sortItem) => (
+                    <DropdownMenuRadioItem
+                      value={sortItem.id}
+                      key={sortItem.id}
+                    >
+                      {sortItem.label}
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingListing;
