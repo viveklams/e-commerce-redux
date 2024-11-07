3b7b2b6bb046e1b8822d5baca0010df9ff1d24f4
viveklams <viveklamgadey@gmail.com>
2024-11-08 00:33:38 +0530

    cart-models done cart-controller <addToCart function> done

---
+++ backend/controllers/shop/cart-controller.js
@@ -0,0 +1,86 @@
+import Cart from "./../../models/Cart.js";
+import Product from "./../../models/Product.js";
+
+export const addToCart = async (req, res) => {
+  try {
+    const { userId, productId, quantity } = req.body;
+
+    if (!userId || !productId || quantity <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid data provided!",
+      });
+    }
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "product not found!",
+      });
+    }
+
+    let cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      cart = new Cart({ userId, items: [] });
+    }
+
+    const findCurrentProductIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+
+    if (findCurrentProductIndex === -1) {
+      cart.items.push({ productId, quantity });
+    } else {
+      cart.items[findCurrentProductIndex].quantity += quantity;
+    }
+
+    await cart.save();
+
+    res.status(200).json({
+      success: true,
+      data: cart,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+
+export const fetchCardItems = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+
+export const updateCartItemQty = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+
+export const deleteCartItem = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
