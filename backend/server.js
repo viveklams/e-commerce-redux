a56a9a98dc6a45e9fdf95e4c145abdaacc51d9a6
viveklams <viveklamgadey@gmail.com>
2024-11-20 12:40:50 +0530

    address models and controllers added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRouter from "../backend/routes/auth/auth-routes.js";
 import adminProductsRouter from "./routes/admin/products-routes.js";
 import shopProductsRouter from "./routes/shop/products-routes.js";
 import shopCartRouter from "./routes/shop/cart-routes.js";
+import shopAddressRouter from "./routes/shop/address-routes.js";
 
 //create database connection
 mongoose
@@ -36,6 +37,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 
