0ebf367ef2371cc266223ce78080d9104d5bf61a
viveklams <viveklamgadey@gmail.com>
2024-11-21 00:08:51 +0530

    changed added with cardFooter Button component for AddressCard

--- frontend/src/components/shopping-view/address-card.jsx
+++ frontend/src/components/shopping-view/address-card.jsx
@@ -1,17 +1,22 @@
 /* eslint-disable react/prop-types */
-import { Card, CardContent } from "../ui/card";
+import { Button } from "../ui/button";
+import { Card, CardContent, CardFooter } from "../ui/card";
 import { Label } from "../ui/label";
 
 function AddressCard({ addressInfo }) {
   return (
     <Card>
       <CardContent className="grid p-4 gap-4">
-        <Label> {addressInfo?.address}</Label>
-        <Label> {addressInfo?.city}</Label>
-        <Label> {addressInfo?.pincode}</Label>
-        <Label> {addressInfo?.phone}</Label>
-        <Label> {addressInfo?.notes}</Label>
+        <Label>Address : {addressInfo?.address}</Label>
+        <Label>City : {addressInfo?.city}</Label>
+        <Label>pincode : {addressInfo?.pincode}</Label>
+        <Label>Phone : {addressInfo?.phone}</Label>
+        <Label>Notes : {addressInfo?.notes}</Label>
       </CardContent>
+      <CardFooter className="p-3 flex justify-between">
+        <Button>Edit</Button>
+        <Button>Delete</Button>
+      </CardFooter>
     </Card>
   );
 }
