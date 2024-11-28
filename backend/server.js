b7adbb54c3195affd1406dcbfc89da4269ce0354
viveklams <viveklamgadey@gmail.com>
2024-11-28 16:34:51 +0530

    search functionality done

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import shopCartRouter from "./routes/shop/cart-routes.js";
 import shopAddressRouter from "./routes/shop/address-routes.js";
 import shopOrderRouter from "./routes/shop/order-routes.js";
 import adminOrderRouter from "./routes/admin/order-routes.js";
+import shopSearchRouter from "./routes/shop/search-routes.js";
 
 //create database connection
 mongoose
@@ -45,5 +46,6 @@ app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/order", shopOrderRouter);
+app.use("/api/shop/search", shopSearchRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT} `));
