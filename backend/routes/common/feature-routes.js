c2fb29fec5f710846f784a2907018b653f4354ed
viveklams <viveklamgadey@gmail.com>
2024-12-07 13:53:16 +0530

    all done foreal

---
+++ backend/routes/common/feature-routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import {
+  addFeatureImage,
+  getFeatureImages,
+} from "../../controllers/common/feature-controller.js";
+
+const router = express.Router();
+
+router.post("/add", addFeatureImage);
+router.get("/get", getFeatureImages);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import shopOrderRouter from "./routes/shop/order-routes.js";
 import adminOrderRouter from "./routes/admin/order-routes.js";
 import shopSearchRouter from "./routes/shop/search-routes.js";
 import shopReviewRouter from "./routes/shop/review-routes.js";
-
+import commonFeatureRouter from "./routes/common/feature-routes.js";
 //create database connection
 mongoose
   .connect(
@@ -49,5 +49,7 @@ app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
 app.use("/api/shop/review", shopReviewRouter);
+
+app.use("/api/common/feature", commonFeatureRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT} `));
